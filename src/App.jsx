033dbf17d9b99802987aa89c93d97fe7d1982d7d
Akib Ali <akib7599@gmail.com>
2023-02-03 02:54:45 +0530

    navbar plus hero section done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { HeroSection } from './compoanents/herosection'
+import { Navbar } from './compoanents/navbar'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+    <Navbar/>
+    <HeroSection/>
+     
+    </div>
+  )
+}
+
+export default App
